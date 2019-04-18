7497e2734354c489bb0190fefb0586f9619c48cd
Austen Collins <austen@serverless.com>
2019-04-17 22:28:38 -0700

    initial version

---
+++ README.md
@@ -0,0 +1,69 @@
+# AWS IAM Policy
+
+Easily provision AWS IAM Policies using [Serverless Components](https://github.com/serverless/components).
+
+&nbsp;
+
+1. [Install](#1-install)
+2. [Create](#2-create)
+3. [Configure](#3-configure)
+4. [Deploy](#4-deploy)
+
+&nbsp;
+
+
+### 1. Install
+
+```shell
+$ npm install -g @serverless/components
+```
+
+### 2. Create
+
+Just create a `serverless.yml` file
+
+```shell
+$ touch serverless.yml
+$ touch .env      # your development AWS api keys
+$ touch .env.prod # your production AWS api keys
+```
+
+the `.env` files are not required if you have the aws keys set globally and you want to use a single stage, but they should look like this.
+
+```
+AWS_ACCESS_KEY_ID=XXX
+AWS_SECRET_ACCESS_KEY=XXX
+```
+
+### 3. Configure
+
+```yml
+# serverless.yml
+
+name: my-app
+
+myRole:
+  component: "@serverless/aws-iam-policy"
+  inputs:
+    name: my-policy
+    description: An AWS IAM Policy created with Serverless Components
+    policy:
+      Version: 2012-10-17
+      Statement:
+        - Effect: Allow
+          Action:
+            - lambda:InvokeFunction
+          Resource: *
+```
+
+### 4. Deploy
+
+```shell
+$ components
+```
+
+&nbsp;
+
+### New to Components?
+
+Checkout the [Serverless Components](https://github.com/serverless/components) repo for more information.
---
+++ serverless.js
@@ -0,0 +1,108 @@
+const { equals, mergeDeepRight } = require('ramda')
+const shortid = require('shortid')
+const aws = require('aws-sdk')
+const { Component } = require('@serverless/components')
+
+/**
+ * AwsIamPolicy
+ * @extends Component
+ */
+
+class AwsIamPolicy extends Component {
+
+  /**
+   * Default
+   * @param  {Object}  [inputs={}] [description]
+   * @return {Promise}             [description]
+   */
+
+  async default(inputs = {}) {
+
+    this.cli.status(`Deploying`)
+
+    // Defaults
+    const defaults = {}
+    defaults.region = 'us-east-1'
+    defaults.policy = {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Effect: 'Allow',
+          Action: [ 'iam:GetPolicyVersion' ],
+          Resource: '*'
+        }
+      ]
+    }
+    defaults.name = `policy-${shortid.generate()}`
+    defaults.description = 'A policy created by Serverless Components'
+    defaults.path = null
+
+    inputs = mergeDeepRight(defaults, inputs)
+
+    // Ensure Document is a string
+    inputs.policy = typeof inputs.policy === 'string' ? inputs.policy : JSON.stringify(inputs.policy)
+
+    const iam = new aws.IAM({ region: inputs.region, credentials: this.context.credentials.aws })
+
+    const params = {
+      PolicyDocument: inputs.policy,
+      PolicyName: inputs.name,
+      Description: inputs.description,
+      Path: inputs.path,
+    }
+
+    let result
+    try {
+      result = await iam.createPolicy(params).promise()
+    } catch(error) {
+      throw new Error(error)
+    }
+
+    // Save state and set outputs
+    const outputs = {}
+    this.state.name = outputs.name = result.Policy.PolicyName
+    this.state.id = outputs.id = result.Policy.PolicyId
+    this.state.arn = outputs.arn = result.Policy.Arn
+    this.state.version = outputs.version = 'v1'
+    this.state.policy = outputs.policy = JSON.parse(inputs.policy)
+    await this.save()
+
+    this.cli.outputs(outputs)
+    return outputs
+  }
+
+  /**
+   * Remove
+   * @param  {Object}  [inputs={}]
+   * @return {Promise}
+   */
+
+  async remove(inputs = {}) {
+
+    if (!this.state.arn) return {}
+
+    const iam = new aws.IAM({
+      region: inputs.region,
+      credentials: this.context.credentials.aws
+    })
+
+    const params = {
+      PolicyArn: this.state.arn
+    }
+
+    let result
+    try {
+      result = await iam.deletePolicy(params).promise()
+    } catch (error) {
+      if (!error.message.includes('does not exist')) throw new Error(error)
+    }
+
+    // Clear state
+    this.state = {}
+    await this.save()
+
+    return {}
+  }
+}
+
+module.exports = AwsIamPolicy
